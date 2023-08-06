fc4bb05ac2324fd55858c705043874df0edb859c
Simon De Backer <simondebacker@msn.com>
2023-08-06 08:25:10 +0200

    MOD example for teleplot
    Teleplot for VSCode
    Plots telemetry sent over Serial
    Every serial message formated >varName:1234\n
    will be ploted in teleplot.
    Other messages will be printed in the teleplot console.

--- example/main.c
+++ example/main.c
@@ -12,7 +12,7 @@ void app_main()
     init_urf(trigger, echo);
     for (;;)
     {
-        printf("lenkte %" PRId32 "µm\n", lenkte_urf(trigger, echo));
+        printf(">lenkte:%" PRId32 "µm\n", lenkte_urf(trigger, echo));
         vTaskDelay(100 / portTICK_RATE_MS);
     }
 }
